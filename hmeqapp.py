97a88aa98c519d1ac48fed465a27508fbe4a720b
scottathan5 <scottdanieltussing@outlook.com>
2025-11-17 12:51:13 -0500

    Update hmeqapp.py

--- hmeqapp.py
+++ hmeqapp.py
@@ -7,8 +7,8 @@ import sklearn  # This is needed for the pickle file to load!
 
 # Load the trained model
 # --- Put the Model in Drive First---
-with open("/Users/scott/OneDrive/Desktop/BUS 458/Streamlit demo/hmeq_model.pkl", "rb") as file:
-    model = pickle.load(file)
+# with open("/Users/scott/OneDrive/Desktop/BUS 458/Streamlit demo/hmeq_model.pkl", "rb") as file:
+#    model = pickle.load(file)
 
 # Title for the app
 # st.title("Home Equity Loan Approval")
